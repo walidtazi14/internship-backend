45efa6432e0c78bde84d3cf709909976a52a762c
Walid Tazi <walidtazi14@gmail.com>
2020-09-16 02:50:51 +0100

    add header

--- index.js
+++ index.js
@@ -12,7 +12,7 @@ var offreRoute = require("./routes/offreRoute");
 var app = express();
 app.use(function(req, res, next) {
     res.header("Access-Control-Allow-Origin", "*");
-    res.header("Access-Control-Allow-Headers", "Origin, X-Requested-With, Content-Type, Accept");
+    res.header("Access-Control-Allow-Headers", "Origin, X-Requested-With, Content-Type, Accept,Authorization");
     next();
   });
 
